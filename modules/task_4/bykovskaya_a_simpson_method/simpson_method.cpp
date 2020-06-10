540a9e53c7da75b62d700ed33a0012c58141704c
alinaut <bykovskaya.alina@gmail.com>
2020-06-10 08:48:04 +0300

    add parallelism

--- modules/task_4/bykovskaya_a_simpson_method/simpson_method.cpp
+++ modules/task_4/bykovskaya_a_simpson_method/simpson_method.cpp
@@ -1,15 +1,12 @@
 // Copyright 2020 Bykovskaya Alina
 #include "../../../modules/task_4/bykovskaya_a_simpson_method/simpson_method.h"
-#include <thread>
-#include <atomic>
 #include <vector>
 #include <functional>
 #include <utility>
-
-static const int countThreads = 2;
+#include <thread>
 
 double calcIntegral(const std::vector<std::pair<double, double>>& scope,
-        std::function<double(const std::vector<double>)> f, size_t accurancy,
+        std::function<double(const std::vector<double>)> f, int accurancy,
         size_t level, std::vector<double> fix_var) {
     if (scope.size() == 0)
         throw "Bad scope";
@@ -20,34 +17,35 @@ double calcIntegral(const std::vector<std::pair<double, double>>& scope,
         throw "Bad level of recursion";
     double h = (scope[level].second - scope[level].first) / (2 * accurancy);
     std::vector<double> func_res(2 * accurancy);
+    int num_threads = std::thread::hardware_concurrency();
+    std::vector<std::thread> threads(num_threads);
+    int delta = (accurancy * 2) / (num_threads);
+    int rem = (accurancy * 2) % (num_threads);
     if (scope.size() == level + 1) {
-        for (size_t i = 0; i < accurancy * 2; i++) {
+        for (int i = 0; i < accurancy * 2; i++) {
             fix_var[level] = scope[level].first + i * h;
             func_res[i] = f(fix_var);
         }
     } else {
-        for (size_t i = 0; i < accurancy * 2; i++) {
-            fix_var[level] = scope[level].first + i * h;
-            func_res[i] =  calcIntegral(scope, f, accurancy, level + 1, fix_var);
-        }
-    }
-    std::atomic<double> res = func_res[0] + func_res[2 * accurancy - 1];
-    std::thread for_thrds[countThreads];
-    for (int j = 0; j < countThreads; ++j) {
-        for_thrds[j] = std::thread([&](){
-            for (size_t i = 0; i < 2 * accurancy - 1; ++i) {
-                if (i % 2 == 1) {
-                    res = res + 4 * func_res[i];
-                } else {
-                    res = res + 2 * func_res[i];
+        for (int i = 0; i < num_threads; ++i) {
+            threads[i] = std::thread([&](int begin, int end, const int id) {
+                if (id == num_threads - 1)
+                    end += rem;
+                for (int j = begin; j < end; ++j) {
+                    fix_var[level] = scope[level].first + j * h;
+                    func_res[j] =  calcIntegral(scope, f, accurancy,
+                                                level + 1, fix_var);
                 }
-            }
-        });
+            }, i * delta, i * delta + delta, i);
+        }
+        for (int i = 0; i < num_threads; ++i)
+            threads[i].join();
     }
-    // for (size_t i = 1; i < 2 * accurancy - 1; i += 2)
-    //     res += 4 * func_res[i];
-    // for (size_t i = 2; i < 2 * accurancy - 2; i += 2)
-    //     res += 2 * func_res[i];
+    double res = func_res[0] + func_res[2 * accurancy - 1];
+    for (int i = 1; i < 2 * accurancy - 1; i += 2)
+        res += 4 * func_res[i];
+    for (int i = 2; i < 2 * accurancy - 2; i += 2)
+        res += 2 * func_res[i];
     res = res * h / 3.0;
     return res;
 }
